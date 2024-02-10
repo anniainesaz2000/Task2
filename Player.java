4981006f48426751d5a036d8d6fe4f4d93ecafd2
anniainesaz2000 <131532952+anniainesaz2000@users.noreply.github.com>
2024-02-10 22:06:56 +0200

    Create Player.java

---
+++ Player.java
@@ -0,0 +1,195 @@
+package bguspl.set.ex;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+
+import bguspl.set.Env;
+
+import java.util.concurrent.BlockingDeque;
+
+/**
+ * This class manages the players' threads and data
+ *
+ * @inv id >= 0
+ * @inv score >= 0
+ */
+public class Player implements Runnable {
+
+    /**
+     * The game environment object.
+     */
+    private final Env env;
+
+    /**
+     * Game entities.
+     */
+    private final Table table;
+
+    /**
+     * The id of the player (starting from 0).
+     */
+    public final int id;
+
+    /**
+     * The thread representing the current player.
+     */
+    private Thread playerThread;
+
+    /**
+     * The thread of the AI (computer) player (an additional thread used to generate key presses).
+     */
+    private Thread aiThread;
+
+    /**
+     * True iff the player is human (not a computer player).
+     */
+    private final boolean human;
+
+    /**
+     * True iff game should be terminated.
+     */
+    private volatile boolean terminate;
+
+    /**
+     * The current score of the player.
+     */
+    private int score;
+
+
+
+    // our new fields
+    private Dealer dealer;
+
+    BlockingQueue<Integer> SetsQueue; // i added this line
+
+    /**
+     * The class constructor.
+     *
+     * @param env    - the environment object.
+     * @param dealer - the dealer object.
+     * @param table  - the table object.
+     * @param id     - the id of the player.
+     * @param human  - true iff the player is a human player (i.e. input is provided manually, via the keyboard).
+     */
+    public Player(Env env, Dealer dealer, Table table, int id, boolean human) {
+        this.env = env;
+        this.table = table;
+        this.id = id;
+        this.human = human;
+
+        this.dealer = dealer;
+        SetsQueue = new ArrayBlockingQueue<Integer>(3);
+    }
+
+    /**
+     * The main player thread of each player starts here (main loop for the player thread).
+     */
+    @Override
+    public void run() {
+        playerThread = Thread.currentThread();
+        env.logger.info("thread " + Thread.currentThread().getName() + " starting.");
+        if (!human) createArtificialIntelligence();
+
+        while (!terminate) {
+            // TODO implement main player loop
+        }
+        if (!human) try { aiThread.join(); } catch (InterruptedException ignored) {}
+        env.logger.info("thread " + Thread.currentThread().getName() + " terminated.");
+    }
+
+    /**
+     * Creates an additional thread for an AI (computer) player. The main loop of this thread repeatedly generates
+     * key presses. If the queue of key presses is full, the thread waits until it is not full.
+     */
+    private void createArtificialIntelligence() {
+        // note: this is a very, very smart AI (!)
+        aiThread = new Thread(() -> {
+            env.logger.info("thread " + Thread.currentThread().getName() + " starting.");
+            while (!terminate) {
+                // TODO implement player key press simulator
+                try {
+                    synchronized (this) { wait(); }
+                } catch (InterruptedException ignored) {}
+            }
+            env.logger.info("thread " + Thread.currentThread().getName() + " terminated.");
+        }, "computer-" + id);
+        aiThread.start();
+    }
+
+    /**
+     * Called when the game should be terminated.
+     */
+    public void terminate() {
+        // TODO implement
+
+    }
+
+    /**
+     * This method is called when a key is pressed.
+     *
+     * @param slot - the slot corresponding to the key pressed.
+     */
+    public void keyPressed(int slot) {   // need to be checked if correct slot number
+        // needed to use with thread
+
+        int card = table.getCard(slot);
+        if(SetsQueue.size() < 3 && SetsQueue.contains(card) == false) {
+            SetsQueue.add(card);
+        } else if (SetsQueue.contains(card) == true) {
+            SetsQueue.remove(card);
+        }
+        if (SetsQueue.size() == 3) {
+            Integer[] testSet = SetsQueue.toArray(new Integer[3]);
+            int[] testSetInt = new int[3];
+            for (int i = 0; i < 3; i++) {
+                testSetInt[i] = testSet[i].intValue();
+            }
+            if (dealer.testSet(testSetInt)) {
+                point();
+            } else {
+                penalty();
+            }
+            SetsQueue.clear();
+        }
+
+    }
+
+    /**
+     * Award a point to a player and perform other related actions.
+     *
+     * @post - the player's score is increased by 1.
+     * @post - the player's score is updated in the ui.
+     */
+    public void point() {//Anni asks a question
+        // TODO implement
+        // the player getting frozen fo 1 second
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+
+        int ignored = table.countCards(); // this part is just for demonstration in the unit tests
+        env.ui.setScore(id, ++score);
+
+        //should we add here dealer.removeCardsFromTable()
+
+
+    }
+
+    /**
+     * Penalize a player and perform other related actions.
+     */
+    public void penalty() {
+        // TODO implement
+
+        try {
+            Thread.sleep(4000);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+    }
+
+    public int score() {
+        return score;
+    }
+}
